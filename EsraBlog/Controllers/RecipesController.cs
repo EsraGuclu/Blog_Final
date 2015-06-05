031c8e0f2697c39afed3a5c1a4bfb7eccd05cc57
Esra <esraguclu22@gmail.com>
2015-06-05 23:38:04 +0300

    Final Step

---
+++ EsraBlog/Controllers/RecipesController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using EsraBlog.Models;
+
+namespace EsraBlog.Controllers
+{
+    public class RecipesController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Recipes
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.AdminPosts.ToListAsync());
+        }
+
+        // GET: Recipes/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            AdminPost adminPost = await db.AdminPosts.FindAsync(id);
+            if (adminPost == null)
+            {
+                return HttpNotFound();
+            }
+            return View(adminPost);
+        }
+
+        // GET: Recipes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Recipes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "ID,Content,Title,Date,Author")] AdminPost adminPost)
+        {
+            if (ModelState.IsValid)
+            {
+                db.AdminPosts.Add(adminPost);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(adminPost);
+        }
+
+        // GET: Recipes/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            AdminPost adminPost = await db.AdminPosts.FindAsync(id);
+            if (adminPost == null)
+            {
+                return HttpNotFound();
+            }
+            return View(adminPost);
+        }
+
+        // POST: Recipes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "ID,Content,Title,Date,Author")] AdminPost adminPost)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(adminPost).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(adminPost);
+        }
+
+        // GET: Recipes/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            AdminPost adminPost = await db.AdminPosts.FindAsync(id);
+            if (adminPost == null)
+            {
+                return HttpNotFound();
+            }
+            return View(adminPost);
+        }
+
+        // POST: Recipes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            AdminPost adminPost = await db.AdminPosts.FindAsync(id);
+            db.AdminPosts.Remove(adminPost);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ EsraBlog/Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace EsraBlog.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<EsraBlog.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            ContextKey = "EsraBlog.Models.ApplicationDbContext";
+        }
+
+        protected override void Seed(EsraBlog.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
